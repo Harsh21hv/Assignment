7b477e80af9fc63d55fd43ab6bafb2ba72849003
Harsh <harshv184@gmail.com>
2024-01-31 18:40:06 +0530

    API fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ export const API_OPTIONS = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: `Beare ${process.env.REACT_APP_TMDBAPI}`,
+    Authorization: `Bearer ${process.env.REACT_APP_TMDBAPI}`,
   },
 };
 
